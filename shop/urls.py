fdf387a12831eee4784188cc5bf1f7c74de207e8
Tolikg63 <ttserunyan@inbox.ru>
2024-02-22 21:13:44 +0400

    Some changes

--- shop/urls.py
+++ shop/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('<int:dish_id>', views.single_page, name='single_page'),
-    path('category/<int:category_id>', views.category_dishes, name='category_dishes'),
-    path('bestsellers', views.bestsellers, name='bestsellers')
+    path('category/<int:category_id>',
+         views.category_dishes, name='category_dishes'),
+    path('bestsellers/', views.bestsellers, name='bestsellers'),
+    path('news/', views.news, name='news')
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from .models import Category, Dishes
 
 def index(request):
@@ -10,6 +11,12 @@ def category_dishes(request, category_id):
     dishes = Dishes.objects.filter(category_id=category_id)
     return render(request, 'shop/category_dishes.html', {'dishes': dishes})
 
+
 def bestsellers(request):
     categories = Category.objects.all()
     return render(request, 'shop/bestsellers.html', {'categories': categories})
+
+
+def news(request):
+    categories = Category.objects.all()
+    return render(request, 'shop/news.html', {'categories': categories})
